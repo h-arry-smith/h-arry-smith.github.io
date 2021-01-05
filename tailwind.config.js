96bd2c2c1ed9615fb80da651c279c4d3be62d2aa
Harry Smith <harry@hsmith.net>
2021-01-05 13:51:37 +0000

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
   },
   theme: {
